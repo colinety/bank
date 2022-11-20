457a82ffc44db9c28890fa99b9c82f8a63215bfc
koki <coline4297@gmail.com>
2022-11-20 20:10:38 +0900

    complite

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ function displayBlock(ele){
 }
 
 const config = {
-        initialForm: document.getElementById("initial-form"),
-        bankPage: document.getElementById("bankPage"),
-        sidePage: document.getElementById("sidePage"),
+    initialForm: document.getElementById("initial-form"),
+    bankPage: document.getElementById("bankPage"),
+    sidePage: document.getElementById("sidePage"),
 }
 
 class BankAccount{
+    maxWithdrawPercent = 0.2;
+    annualInterestRate = 0.08;
+
     constructor(firstName, lastName, email, type, accountNumber, money) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -27,6 +30,29 @@ class BankAccount{
 
     getFullName(){
         return this.firstName + " " + this.lastName;
+    }
+
+    calculateWithdrawAmount(amount){
+        let maxWithdrawAmount = Math.floor(this.money * this.maxWithdrawPercent);
+        amount = amount > maxWithdrawAmount ? maxWithdrawAmount : amount;
+        return amount;
+    }
+
+    withdraw(amount){
+        this.money -= this.calculateWithdrawAmount(amount);
+        return this.calculateWithdrawAmount(amount);
+    }
+
+    deposit(amount){
+        this.money += amount;
+        return amount;
+    }
+
+    simulateTimePassage(days){
+        let daysPerYear = 365;
+        let profit = (this.money * Math.pow(1 + this.annualInterestRate, days/daysPerYear)) - this.money;
+        this.money += profit;
+        return profit;
     }
 }
 
@@ -100,9 +126,20 @@ function mainBankPage(bankAccount) {
         </div>
     `;
 
-    menuCon.querySelectorAll("#withdrawBtn")[0].addEventListener("click", function(){withdrawController()});
-    menuCon.querySelectorAll("#depositBtn")[0].addEventListener("click", function(){alert("deposit")});
-    menuCon.querySelectorAll("#comeBackLaterBtn")[0].addEventListener("click", function(){alert("come back later")});
+    menuCon.querySelectorAll("#withdrawBtn")[0].addEventListener("click", function(){
+        sideBankSwitch();
+        config.sidePage.append(withdrawPage(bankAccount));
+    });
+
+    menuCon.querySelectorAll("#depositBtn")[0].addEventListener("click", function(){
+        sideBankSwitch();
+        config.sidePage.append(depositPage(bankAccount));
+    });
+
+    menuCon.querySelectorAll("#comeBackLaterBtn")[0].addEventListener("click", function(){
+        sideBankSwitch();
+        config.sidePage.append(comeBackLaterPage(bankAccount));
+    });
 
     let container = document.createElement("div");
     container.append(infoCon, balanceCon, menuCon);
@@ -118,41 +155,41 @@ function billInputSelector(title) {
         <div class="form-group row">
             <label for="moneyWithdraw100" class="col-2 col-form-label col-form-label-sm">$100</label>
             <div class="col-10">
-                <input type="number" class="form-control form-control-sm text-right withdraw-bill" data-bill="100" id="moneyWithdraw100" placeholder="5">
+                <input type="number" class="form-control form-control-sm text-right withdraw-bill bill-input" data-bill="100" id="moneyWithdraw100" placeholder="5">
             </div>
         </div>
         <div class="form-group row">
             <label for="moneyWithdraw50" class="col-2 col-form-label col-form-label-sm">$50</label>
             <div class="col-10">
-                <input type="number" class="form-control form-control-sm text-right withdraw-bill" data-bill="50" id="moneyWithdraw50" placeholder="1">
+                <input type="number" class="form-control form-control-sm text-right withdraw-bill bill-input" data-bill="50" id="moneyWithdraw50" placeholder="1">
             </div>
         </div>
         <div class="form-group row">
             <label for="moneyWithdraw20" class="col-2 col-form-label col-form-label-sm">$20</label>
             <div class="col-10">
-                <input type="number" class="form-control form-control-sm text-right withdraw-bill" data-bill="20" id="moneyWithdraw20" placeholder="2">
+                <input type="number" class="form-control form-control-sm text-right withdraw-bill bill-input" data-bill="20" id="moneyWithdraw20" placeholder="2">
             </div>
         </div>
         <div class="form-group row">
             <label for="moneyWithdraw10" class="col-2 col-form-label col-form-label-sm">$10</label>
             <div class="col-10">
-                <input type="number" class="form-control form-control-sm text-right withdraw-bill" data-bill="10" id="moneyWithdraw10" placeholder="3">
+                <input type="number" class="form-control form-control-sm text-right withdraw-bill bill-input" data-bill="10" id="moneyWithdraw10" placeholder="3">
             </div>
         </div>
         <div class="form-group row">
             <label for="moneyWithdraw5" class="col-2 col-form-label col-form-label-sm">$5</label>
             <div class="col-10">
-                <input type="number" class="form-control form-control-sm text-right withdraw-bill" data-bill="5" id="moneyWithdraw5" placeholder="1">
+                <input type="number" class="form-control form-control-sm text-right withdraw-bill bill-input" data-bill="5" id="moneyWithdraw5" placeholder="1">
             </div>
         </div>
         <div class="form-group row">
             <label for="moneyWithdraw1" class="col-2 col-form-label col-form-label-sm">$1</label>
             <div class="col-10">
-                <input type="number" class="form-control form-control-sm text-right withdraw-bill" data-bill="1" id="moneyWithdraw1" placeholder="4">
+                <input type="number" class="form-control form-control-sm text-right withdraw-bill bill-input" data-bill="1" id="moneyWithdraw1" placeholder="4">
             </div>
         </div>
         <div class="text-center money-box p-3">
-            <p id="withdrawTotal">$0.00</p>
+            <p id="totalBillAmount">$0.00</p>
         </div>
     `;
     return container;
@@ -160,29 +197,35 @@ function billInputSelector(title) {
 
 function backNextBtn(backString, nextString){
     let container = document.createElement("div");
+
     container.innerHTML =
     `
     <div class="d-flex justify-content-between">
         <div class="col-6 pl-0">
-            <button class="btn btn-outline-primary col-12">${backString}</button>
+            <button class="btn btn-outline-primary col-12 back-btn">${backString}</button>
         </div>
         <div class="col-6 pr-0">
-            <button class="btn btn-primary col-12">${nextString}</button>
+            <button class="btn btn-primary col-12 next-btn">${nextString}</button>
         </div>
     </div>
     `
     return container;
 }
 
-function withdrawController() {
+function sideBankSwitch(){
     displayNone(config.bankPage);
     displayBlock(config.sidePage);
     config.bankPage.innerHTML = "";
     config.sidePage.innerHTML = "";
-    config.sidePage.append(withdrawPage());
 }
 
-function withdrawPage(){
+function bankReturn(bankAccount){
+    displayNone(config.sidePage);
+    displayBlock(config.bankPage);
+    config.bankPage.append(mainBankPage(bankAccount));
+}
+
+function withdrawPage(bankAccount){
     let container = document.createElement("div");
     container.classList.add("p-5");
 
@@ -191,6 +234,190 @@ function withdrawPage(){
 
     withdrawContainer.append(billInputSelector("Please Enter The Withdrawal Amount"));
     withdrawContainer.append(backNextBtn("back", "next"));
+
+    let backBtn = withdrawContainer.querySelectorAll(".back-btn").item(0);
+    backBtn.addEventListener("click", function(){
+        bankReturn(bankAccount);
+    })
+
+    let billInputs = withdrawContainer.querySelectorAll(".bill-input");
+
+    for (let i = 0; i < billInputs.length; i++) {
+        billInputs[i].addEventListener("change", function(event){
+            document.getElementById("totalBillAmount").innerHTML = billSummation(billInputs, "data-bill").toString();
+        });
+    }
+
+    let nextBtn = withdrawContainer.querySelectorAll(".next-btn").item(0);
+    nextBtn.addEventListener("click", function(){
+        container.innerHTML = '';
+
+        let confirmDialog = document.createElement("div");
+        confirmDialog.append(billDialog("The money you are going to take is ...", billInputs, "data-bill"));
+        container.append(confirmDialog);
+
+        let total = billSummation(billInputs, "data-bill");
+
+        confirmDialog.innerHTML +=
+        `
+            <div class="d-flex bg-danger py-1 py-md-2 mb-3 text-white">
+                <p class="col-8 text-left rem1p5">Total to be withdrawn: </p>
+                <p class="col-4 text-right rem1p5">$${bankAccount.calculateWithdrawAmount(total)}</p>
+            </div>
+        `
+
+        let withdrawConfirmBtns = backNextBtn("Go Back", "Confirm");
+        confirmDialog.append(withdrawConfirmBtns);
+
+        let confirmBackBtn = withdrawConfirmBtns.querySelectorAll(".back-btn")[0];
+        let confirmNextBtn = withdrawConfirmBtns.querySelectorAll(".next-btn")[0];
+
+        confirmBackBtn.addEventListener("click", function(){
+            container.innerHTML = "";
+            container.append(withdrawContainer);
+        });
+
+        confirmNextBtn.addEventListener("click", function(){
+            bankAccount.withdraw(total);
+            bankReturn(bankAccount);
+        });
+    })
+
+    return container;
+}
+
+function billSummation(inputElementNodeList, multiplierAttribute){
+    let summation = 0;
+    for(let i = 0; i < inputElementNodeList.length; i++){
+        let currEle = inputElementNodeList[i];
+        let value = parseInt(currEle.value);
+
+        value = currEle.hasAttribute(multiplierAttribute) ? parseInt(currEle.getAttribute(multiplierAttribute)) * value : value;
+        summation += value >= 0 ? value : 0;
+    }
+    return summation;
+}
+
+function billDialog(title, inputElementNodeList, multiplierAttribute){
+    let container = document.createElement("div");
+
+    let billElements = '';
+    for (let i = 0; i < inputElementNodeList.length; i++){
+        let value = parseInt(inputElementNodeList[i].value);
+
+        if (value > 0) {
+            let bill = "$" + inputElementNodeList[i].getAttribute(multiplierAttribute);
+            billElements += `<p class="rem1p3 calculation-box mb-1 pr-2">${value} × ${bill}</p>`
+        }
+    }
+
+    let totalString = `<p class="rem1p3 pr-2">total: $${billSummation(inputElementNodeList, multiplierAttribute)}</p>`;
+
+    container.innerHTML =
+    `
+        <h2 class="pb-1">${title}</h2>
+        <div class="d-flex justify-content-center">
+            <div class="text-right col-8 px-1 calculation-box">
+                ${billElements}
+                ${totalString}
+            </div>
+        </div>
+    `
+    return container;
+}
+
+function depositPage(bankAccount){
+    let container = document.createElement("div");
+    container.classList.add("p-5");
+
+    let depositContainer = document.createElement("div");
+    container.append(depositContainer);
+
+    depositContainer.append(billInputSelector("Please Enter The Deposit Amount"));
+    depositContainer.append(backNextBtn("back", "next"));
+
+    let backBtn = depositContainer.querySelectorAll(".back-btn").item(0);
+    backBtn.addEventListener("click", function(){
+        bankReturn(bankAccount);
+    })
+
+    let billInputs = depositContainer.querySelectorAll(".bill-input");
+
+    for (let i = 0; i < billInputs.length; i++) {
+        billInputs[i].addEventListener("change", function(event){
+            document.getElementById("totalBillAmount").innerHTML = billSummation(billInputs, "data-bill").toString();
+        });
+    }
+
+    let nextBtn = depositContainer.querySelectorAll(".next-btn").item(0);
+
+    nextBtn.addEventListener("click", function(){
+        container.innerHTML = '';
+
+        let confirmDialog = document.createElement("div");
+        confirmDialog.append(billDialog("The money you are going to deposit is ...", billInputs, "data-bill"));
+        container.append(confirmDialog);
+
+        let total = billSummation(billInputs, "data-bill");
+        confirmDialog.innerHTML +=
+        `
+            <div class="d-flex bg-danger py-1 py-md-2 mb-3 text-white">
+                <p class="col-8 text-left rem1p5">Total to be withdrawn: </p>
+                <p class="col-4 text-right rem1p5">$${total}</p>
+            </div>
+        `
+        let depositConfirmBtns = backNextBtn("Go Back", "Confirm");
+        confirmDialog.append(depositConfirmBtns);
+
+        let confirmBackBtn = depositConfirmBtns.querySelectorAll(".back-btn")[0];
+        let confirmNextBtn = depositConfirmBtns.querySelectorAll(".next-btn")[0];
+
+        confirmBackBtn.addEventListener("click", function(){
+            container.innerHTML = "";
+            container.append(depositContainer);
+        });
+
+        confirmNextBtn.addEventListener("click", function(){
+            bankAccount.deposit(total);
+            bankReturn(bankAccount);
+        });
+
+    });
+
+    return container;
+}
+
+function comeBackLaterPage(bankAccount){
+    let container = document.createElement("div");
+    container.classList.add("p-5");
+
+    container.innerHTML =
+    `
+        <div class="p-5">
+            <h2 class="pb-3">How many days will you be gone?</h2>
+            <div class="form-group">
+                <input type="number" class="form-control" id="days-gone" placeholder="4">
+            </div>
+        </div>
+    `
+
+    container.append(backNextBtn("Back", "Confirm"));
+
+    let backBtn = container.querySelectorAll(".back-btn")[0];
+
+    backBtn.addEventListener("click", function(){
+        bankReturn(bankAccount);
+    })
+
+    let nextBtn = container.querySelectorAll(".next-btn")[0];
+
+    nextBtn.addEventListener("click", function(){
+        let daysGoneInput = container.querySelectorAll("#days-gone")[0];
+        let totalDaysGone = parseInt(daysGoneInput.value);
+        totalDaysGone = totalDaysGone > 0 ? totalDaysGone : 0;
+        bankAccount.simulateTimePassage(totalDaysGone);
+        bankReturn(bankAccount);
+    })
 
     return container;
 }
